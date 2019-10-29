f698f1baaef5aace4ec46c01d61d0541db855e58
AB055474 <AB055474zhangdongbin@ab-insurance.com>
2019-10-29 16:22:02 +0800

    2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public  static  void  main(String[] args){
              System.out.println("1");
              System.out.println("2");
+             System.out.println("4");
          }
 
 }
